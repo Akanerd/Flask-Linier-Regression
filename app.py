ab4714b6342690a62d839b990d2e247e0401e78c
Muhammad Hafizh Azzasafah <85096618+Akanerd@users.noreply.github.com>
2022-06-11 01:05:43 +0700

    Update tampilan UI

--- app.py
+++ app.py
@@ -6,16 +6,20 @@ app = Flask(__name__)
 
 
 @app.route("/", methods=['GET', 'POST'])
-def iris_prediction():
+def main():
+    return render_template("jenisKelamin.html")
+
+@app.route("/generic.html", methods=['GET', 'POST'])
+def generic():
     if request.method == 'GET':
-        return render_template("jenisKelamin.html")
+        return render_template("generic.html")
     elif request.method == 'POST':
         gender = float(request.form['gender'])
         height = float(request.form['height'])
         input_array = [[height, gender]]
         weight_predictor_model = pickle.load(open('model-development/weight_predictor.pkl', 'rb'))
         result = round(weight_predictor_model.predict(input_array)[0], 2)
-        return render_template('jenisKelamin.html', result=result)
+        return render_template('generic.html', result=result)
     else:
         return "Unsupported Request Method"
 
